81a2b963dde29e6037bd9e0f7d6ffdd77ab60a67
Iskender <iska.satybaldyev@gmail.com>
2018-04-14 13:45:14 +0300

    Add css style

--- public/js/index.js
+++ public/js/index.js
@@ -34,13 +34,15 @@ locationButton.on('click',function(){
   if(!navigator.geolocation){
     return alert('Geolocation not supported by your browser !')
   }
+  locationButton.attr('disabled','disabled').text('Sending location...')
   navigator.geolocation.getCurrentPosition(function(position){
-    console.log(position)
+    locationButton.removeAttr('disabled').text('Send location')
     socket.emit('createLocationMessage',{
       latitude : position.coords.latitude,
       longitude : position.coords.longitude,
     })
   },function(){
+    locationButton.removeAttr('disabled').text('Send location')
     alert('Unable to fetch location.')
   })
 })
